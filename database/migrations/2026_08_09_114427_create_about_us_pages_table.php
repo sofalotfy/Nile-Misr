147c726875ef3dd042e04eb78f2023dff20c98d4
sofalotfy <lotfysofa@gmail.com>
2026-08-11 16:27:46 +0300

    aa

--- database/migrations/2026_08_09_114427_create_about_us_pages_table.php
+++ database/migrations/2026_08_09_114427_create_about_us_pages_table.php
@@ -15,75 +15,74 @@ return new class extends Migration
             $table->id();
 
             // Hero Section
-            $table->text("hero-image");
-            $table->string("title");
-            $table->string("subtitle");
+            $table->text("hero-image")->nullable();
+            $table->string("title")->nullable();
+            $table->string("subtitle")->nullable();
 
             // Origins and Beginnings Section
-            $table->string("beginning-tag");
-            $table->text("beginning-tittle");
-            $table->text("beginning-text");
+            $table->string("beginning-tag")->nullable();
+            $table->text("beginning-tittle")->nullable();
+            $table->text("beginning-text")->nullable();
 
             // Our vision Section
-            $table->string("our-vision-tag");
-            $table->text("our-vision-vertical-tag");
-            $table->text("our-vision-tittle");
-            $table->text("our-vision-text");
-            $table->text("our-vision-image");
+            $table->string("our-vision-tag")->nullable();
+            $table->text("our-vision-vertical-tag")->nullable();
+            $table->text("our-vision-tittle")->nullable();
+            $table->text("our-vision-text")->nullable();
+            $table->text("our-vision-image")->nullable();
 
             //Our Mission Section
-            $table->string("our-mission-tag");
-            $table->text("our-mission-tittle");
-            $table->text("our-mission-text");
-            $table->text("our-mission-image");
-            $table->text("our-mission-floating-image");
+            $table->string("our-mission-tag")->nullable();
+            $table->text("our-mission-tittle")->nullable();
+            $table->text("our-mission-text")->nullable();
+            $table->text("our-mission-image")->nullable();
+            $table->text("our-mission-floating-image")->nullable();
 
             // Values
-            $table->string("values-tag");
-            $table->text("values-title");
-            $table->string("respecting-sanctity-title");
-            $table->text("respecting-sanctity-text");
-            $table->string("integrity-and-credibility-tittle");
-            $table->text("integrity-and-credibility-text");
-            $table->string("experience-and-professionalism-tittle");
-            $table->text("experience-and-professionalism-text");
-            $table->string("comfort-and-safety-tittle");
-            $table->text("comfort-and-safety-text");
-            $table->string("innovation-and-evelopment-tittle");
-            $table->text("innovation-and-evelopment-text");
-            $table->string("service-excellence-tittle");
-            $table->text("service-excellence-text");
-            $table->string("egyptian-authenticity-tittle");
-            $table->text("egyptian-authenticity-text");
+            $table->string("values-tag")->nullable();
+            $table->text("values-title")->nullable();
+            $table->string("respecting-sanctity-title")->nullable();
+            $table->text("respecting-sanctity-text")->nullable();
+            $table->string("integrity-and-credibility-tittle")->nullable();
+            $table->text("integrity-and-credibility-text")->nullable();
+            $table->string("experience-and-professionalism-tittle")->nullable();
+            $table->text("experience-and-professionalism-text")->nullable();
+            $table->string("comfort-and-safety-tittle")->nullable();
+            $table->text("comfort-and-safety-text")->nullable();
+            $table->string("innovation-and-evelopment-tittle")->nullable();
+            $table->text("innovation-and-evelopment-text")->nullable();
+            $table->string("service-excellence-tittle")->nullable();
+            $table->text("service-excellence-text")->nullable();
+            $table->string("egyptian-authenticity-tittle")->nullable();
+            $table->text("egyptian-authenticity-text")->nullable();
 
             // Nile Journey
-            $table->string("nile-journey-tag");
-            $table->text("nile-journey-tittle");
-            $table->text("nile-journey-text");
-            $table->text("nile-journey-image");
-            $table->text("nile-journey-floating-image");
+            $table->string("nile-journey-tag")->nullable();
+            $table->text("nile-journey-tittle")->nullable();
+            $table->text("nile-journey-text")->nullable();
+            $table->text("nile-journey-image")->nullable();
+            $table->text("nile-journey-floating-image")->nullable();
 
             // Achievements
-            $table->text("achievements-tag");
-            $table->text("achievements-tittle");
-            $table->text("achievements-text");
-            $table->text("achievements-years-nummber");
-            $table->text("achievements-year");
-            $table->json("achievements")->nullable; // Include number,tittle and text
+            $table->text("achievements-quote")->nullable();
+            $table->text("achievements-tittle")->nullable();
+            $table->text("achievements-text")->nullable();
+            $table->integer("achievements-years-count")->nullable();
+            $table->json("achievements")->nullable(); // Include tittle and text
 
             // Timeline
-            $table->string("tmeline-tag");
-            $table->text("tmeline-tittle");
-            $table->json("tmeline")->nullable;
+            $table->string("tmeline-tag")->nullable();
+            $table->text("tmeline-tittle")->nullable();
+            $table->json("tmeline")->nullable(); // array of dictionaries each include year and an array of events strings
 
             // Team
-            $table->string("team-taag");
-            $table->text("team-tittle");
-            $table->text("team-text");
-            $table->text("team-image");
+            $table->string("team-tag")->nullable();
+            $table->text("team-title")->nullable();
+            $table->text("team-text")->nullable();
+            $table->text("team-image")->nullable();
 
             // Video
-            $table->text("video");
+            $table->text("video")->nullable();
 
 
             $table->timestamps();
