6139ebe83700c0db8a02d75697c8b21f7f6ee6dd
abdelrahman196 <abdelrahmanhelmy195@gmail.com>
2026-08-12 17:32:05 +0300

    .

--- resources/views/moderators.blade.php
+++ resources/views/moderators.blade.php
@@ -32,13 +32,14 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4 md-mb-20px sm-mb-0" data-anime='{ "el": "childs", "translateY": [15, 0], "opacity": [0,1], "duration": 600, "delay": 0, "staggervalue": 300, "easing": "easeOutQuad" }'>
-                <h2 class="text-dark-gray fw-500 ls-minus-2px alt-font">{!! $pageData[""] !!}مشرفين نيل مصر</h2>
-                <span class="text-tussock-yellow fs-20 fw-600 d-inline-block mb-5px">علم مضيء في سماء السياحة الدينية</span>
+                <h2 class="text-dark-gray fw-500 ls-minus-2px alt-font">{!! $pageData["our-supervisors-tag"]?? "مشرفين نيل مصر" !!}</h2>
+                <span class="text-tussock-yellow fs-20 fw-600 d-inline-block mb-5px">{!! $pageData["our-supervisors-title"]?? "علم مضيء في سماء السياحة الدينية" !!}</span>
                 <img src="imgs/arrow.png" class="position-relative mt-5" alt="" data-bottom-top="transform: rotate(120deg);" data-top-bottom="transform: rotate(0);" />
             </div>
             <div class="col-lg-8" data-anime='{ "el": "childs", "translateY": [15, 0], "opacity": [0,1], "duration": 600, "delay": 0, "staggervalue": 300, "easing": "easeOutQuad" }'>
                 <div class="row">
                     <div class="last-paragraph-no-margin">
+                        {!! $pageData["our-supervisors-description"]?? "
                         <p>تعد خدمة الإشراف خلال تأدية الحاج و المعتمر لرحلته المقدسة
                             خارج أرض الوطن هي العمل الأسمى على الإطلاق لنيل
                             مصر للسياحة .. و هو العنصر الأساسي لنجاح الشركة و إتمام
@@ -51,6 +52,7 @@
                             لحظات الرحلة و في كل مشهد وموقف للمناسك و الأركان
                             سواء بسواء لجميع العملاء.
                         </p>
+                        " !!}
                         <span class="text-tussock-yellow fs-20 fw-600 d-inline-block mt-30px">وتشمل الخدمة ما يلي :</span>
                     </div>
                 </div>
@@ -70,15 +72,17 @@
                     <div class="tab-pane fade in active show" id="tab_eight1">
                         <div class="row align-items-center justify-content-center g-0">
                             <div class="col-lg-6 md-mb-30px position-relative">
-                                <img src="imgs/org.jpg" alt="" class="w-100">
+                                <img src="{{  $pageData['organizational-supervisors-image']?asset("storage/" . $pageData['organizational-supervisors-image']):asset("imgs/org.jpg")   }}" alt="" class="w-100">
                                 <!-- <div class="position-absolute top-30px xs-top-0px left-minus-80px lg-left-minus-50px xs-left-minus-0px xs-w-130px"><img src="images/demo-hotel-and-resort-amenities-07.png" alt="" /></div> -->
                             </div>
                             <div class="col-lg-5 offset-lg-1">
                                 <!-- <span class="text-tussock-yellow fs-20 fw-600 d-inline-block mb-10px">الإشراف الإداري والتنظيمي</span> -->
-                                <h3 class="alt-font fw-500 text-dark-gray">الإشراف الإداري والتنظيمي</h3>
+                                <h3 class="alt-font fw-500 text-dark-gray">{!! $pageData['organizational-supervisors-title']?? "الإشراف الإداري والتنظيمي" !!}</h3>
+                                {!! $pageData['organizational-supervisors-text']?? "
                                 <p class="mb-30px">
                                     لأيتولى فريقنا المتخصص إدارة جميع الجوانب الإدارية للرحلة من إعداد الوثائق والتصاريح اللازمة إلى التنسيق مع السلطات السعودية والجهات المختصة. نحرص على متابعة جداول الرحلات والانتقالات بين المشاعر المقدسة بدقة عالية، مع إدارة الحجوزات وتوزيع الغرف بما يضمن راحة جميع الحجاج. كما نقوم بالتأكد من استكمال جميع إجراءات الدخول والخروج وتوزيع بطاقات الهوية والأساور الخاصة لضمان سير منظم للرحلة.
                                 </p>
+                                " !!}
                             </div>
                         </div>
                     </div>
@@ -99,17 +103,17 @@
                     <div class="tab-pane fade in active show" id="tab_eight1">
                         <div class="row align-items-center justify-content-center g-0">
                             <div class="col-lg-6 md-mb-30px position-relative">
-                                <img src="imgs/logi.JPG" alt="" class="w-100">
+                                <img src="{{ $pageData['service-supervisors-image']?asset("storage/" . $pageData['service-supervisors-image']):asset("imgs/logi.JPG") }}" alt="" class="w-100">
                                 <!-- <div class="position-absolute top-30px xs-top-0px left-minus-80px lg-left-minus-50px xs-left-minus-0px xs-w-130px"><img src="images/demo-hotel-and-resort-amenities-07.png" alt="" /></div> -->
                             </div>
                             <div class="col-lg-5 offset-lg-1">
                                 <!-- <span class="text-tussock-yellow fs-20 fw-600 d-inline-block mb-10px">الإشراف اللوجستي والخدمي</span> -->
-                                <h3 class="alt-font fw-500 text-dark-gray">الإشراف اللوجستي والخدمي</h3>
-                                <p class="mb-30px">نتولى الإشراف الكامل على جودة أماكن الإقامة في مكة والمدينة
-                                    مع متابعة خدمات التنظيف والصيانة وإدارة وجبات الطعام لضمان أعلى معايير الجودة،
-                                    نوفر خدمات نقل متميزة بحافلات مكيفة ومجهزة بأحدث المعدات لضمان راحة الحجاج أثناء التنقل
-                                    بين المشاعر المقدسة. كما نحرص على توفير المياه والمرطبات في جميع الأوقات
-                                    مع مرافقة الحجاج في جميع التنقلات والالتزام قدر المستطاع بالمواعيد المحددة لأداء المناسك.
+                                <h3 class="alt-font fw-500 text-dark-gray">{!! $pageData['service-supervisors-title']?? "الإشراف اللوجستي والخدمي" !!}</h3>
+                                {!! $pageData['service-supervisors-text']?? "نتولى الإشراف الكامل على جودة أماكن الإقامة في مكة والمدينة
+                                مع متابعة خدمات التنظيف والصيانة وإدارة وجبات الطعام لضمان أعلى معايير الجودة，
+                                نوفر خدمات نقل متميزة بحافلات مكيفة ومجهزة بأحدث المعدات لضمان راحة الحجاج أثناء التنقل
+                                بين المشاعر المقدسة. كما نحرص على توفير المياه والمرطبات في جميع الأوقات
+                                مع مرافقة الحجاج في جميع التنقلات والالتزام قدر المستطاع بالمواعيد المحددة لأداء المناسك."!!}
                                 </p>
                             </div>
                         </div>
@@ -127,13 +131,15 @@
         <div class="row align-items-center mb-8 md-mb-15 xs-mb-20">
             <div class="col-lg-5 md-mb-50px" data-anime='{ "el": "childs", "opacity": [0, 1], "rotateY": [-90, 0], "rotateZ": [-10, 0], "translateY": [80, 0], "translateZ": [50, 0], "staggervalue": 200, "duration": 800, "delay": 200, "easing": "easeOutCirc" }'>
                 <!-- <span class="text-tussock-yellow fs-20 fw-600 d-inline-block mb-10px">الإشراف الديني والإرشادي</span> -->
-                <h3 class="alt-font text-dark-gray ls-minus-2px">الإشراف الديني والإرشادي</h3>
+                <h3 class="alt-font text-dark-gray ls-minus-2px">{!! $pageData["religious-supervisors-title"]??"الإشراف الديني والإرشادي" !!}</h3>
+                {!! $pageData["religious-supervisors-text"]??"
                 <p class="w-80 xl-w-100 mb-35px xs-mb-10px">مشرفون دينيون متخصصون لشرح مناسك الحج والعمرة وفقًاً للسنة النبوية الصحيحة، مع إرشاد الحجاج خلال أداء المناسك في المواقيت المحددة والإجابة على جميع الاستفسارات الفقهية والدينية. نقدم جلسات تحضيرية قبل بداية المناسك ومحاضرات دينية توعوية، مع مرافقة شخصية للحجاج أثناء الطواف والسعي وفي عرفات ومزدلفة ومنى. هدفنا هو ضمان أداء المناسك بالطريقة الصحيحة والمثلى لتحقيق أقصى استفادة روحانية من هذه التجربة المقدسة.
                 </p>
+                " !!}
             </div>
             <div class="col-lg-6 offset-lg-1 position-relative">
                 <div class="overflow-hidden position-relative xs-w-80 float-end" data-anime='{ "effect": "slide", "color": "#A0875B", "direction":"rl", "easing": "easeOutQuad", "duration": 600, "delay":400}'>
-                    <img class="w-100" src="imgs/real.jpg" alt="">
+                    <img class="w-100" src="{{  $pageData['religious-supervisors-image']?asset("storage/" . $pageData['religious-supervisors-image']):asset("imgs/real.jpg")   }}" alt="">
                 </div>
             </div>
         </div>
@@ -152,13 +158,15 @@
                         <div class="row align-items-center justify-content-center g-0">
                             <div class="col-lg-5">
                                 <!-- <span class="text-tussock-yellow fs-20 fw-600 d-inline-block mb-10px">الإشراف اللوجستي والخدمي</span> -->
-                                <h3 class="alt-font fw-500 text-dark-gray">إدارة الطوارئ والأمان</h3>
+                                <h3 class="alt-font fw-500 text-dark-gray">{!! $pageData["safety-management-title"]?? "إدارة الطوارئ والأمان" !!}</h3>
+                                {!! $pageData["safety-management-text"]?? "
                                 <p class="mb-30px">
                                     نتولى الإشراف الكامل على جودة أماكن يضم فريقنا متخصصين في إدارة المخاطر والطوارئ لوضع خطط بديلة للتعامل مع أي ظروف استثنائية قد تطرأ، مع التواصل المستمر مع السلطات الأمنية السعودية. نطبق أحدث بروتوكولات السلامة المعتمدة دوليًاً مع متابعة دقيقة للحجاج في الأماكن المزدحمة وتوفير أرقام طوارئ للتواصل المستمر. نوفر تأمينًاً شاملًاً ضد جميع المخاطر مع تدريب مستمر لفريق العمل على التعامل مع الحالات الطارئة لضمان سلامة وأمان جميع الحجاج.
                                 </p>
+                                " !!}
                             </div>
                             <div class="col-lg-6 offset-lg-1 md-mb-30px position-relative">
-                                <img src="imgs/safe.JPG" alt="" class="w-100">
+                                <img src="{{  $pageData['safety-management-image']?asset("storage/" . $pageData['safety-management-image']):asset("imgs/safe.JPG")   }}" alt="" class="w-100">
                                 <!-- <div class="position-absolute top-30px xs-top-0px left-minus-80px lg-left-minus-50px xs-left-minus-0px xs-w-130px"><img src="images/demo-hotel-and-resort-amenities-07.png" alt="" /></div> -->
                             </div>
 
