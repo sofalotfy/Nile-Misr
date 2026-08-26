4178bcf7f243e0d9afd31d17cdfdda088a48f535
sofalotfy <lotfysofa@gmail.com>
2026-08-26 12:55:02 +0300

    as

--- app/Filament/Resources/HomePages/Schemas/HomePageForm.php
+++ app/Filament/Resources/HomePages/Schemas/HomePageForm.php
@@ -7,6 +7,7 @@ use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use App\Enums\SitePages;
