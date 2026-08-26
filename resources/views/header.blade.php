92841f61556359735d049e2b490797b93170dc6e
sofalotfy <lotfysofa@gmail.com>
2026-08-26 15:43:22 +0300

    as

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -61,172 +61,157 @@
 
             <!-- start navigation -->
             <nav
-                class="navbar navbar-expand-lg header-light bg-white header-reverse glass-effect"
-                data-header-hover="light"
+                class="navbar navbar-expand-lg header-light bg-white top-logo disable-fixed header-demo"
             >
 
-                <div class="container-fluid">
+                <div class="container-fluid flex-wrap">
 
-                    <!-- Logo -->
-                    <div class="col-auto col-lg-2 me-lg-0 me-auto">
+                    <div class="col-12 col-sm-12 col-lg-8 p-0 mx-lg-auto d-flex align-items-center justify-content-between">
 
-                        <a class="navbar-brand section-link" href="{{ route('home') }}">
+                        <!-- Left: Hotline + Toggler -->
+                        <div class="col col-lg-2">
 
-                            <img
-                                src="{{ asset('storage/' . $siteData['header-logo']) }}"
-                                data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}"
-                                alt="Nile Misr"
-                                class="default-logo"
+                            <!-- Hotline (desktop only, hides at 1150px) -->
+                            <div class="header-icon header-desktop-only">
+
+                                <div>
+                                    <div class="header-icon">
+                                        <div class="me-25px">
+                                            <div class="fs-15 xl-fs-13 widget-text fw-500">
+                                                <span class="w-35px h-35px bg-yellow d-inline-flex align-items-center justify-content-center me-10px border-radius-100px">
+                                                    <i class="feather icon-feather-phone"></i>
+                                                </span>
+                                                <a
+                                                    href="tel:+{{$siteData['hotline']}}"
+                                                    class="widget-text text-white-hover fw-600 fs-18"
+                                                >
+                                                    {{$siteData['hotline']}}
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Navbar Toggler (mobile) -->
+                            <button
+                                class="navbar-toggler float-start"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarNav"
+                                aria-controls="navbarNav"
+                                aria-label="Toggle navigation"
                             >
+                                <span class="navbar-toggler-line"></span>
+                                <span class="navbar-toggler-line"></span>
+                                <span class="navbar-toggler-line"></span>
+                                <span class="navbar-toggler-line"></span>
+                            </button>
 
-                            <img
-                                src="{{ asset('storage/' . $siteData['header-logo']) }}"
-                                data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}"
-                                alt="Nile Misr"
-                                class="alt-logo"
-                            >
+                        </div>
 
-                            <img
-                                src="{{ asset('storage/' . $siteData['header-logo']) }}"
-                                data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}"
-                                alt="Nile Misr"
-                                class="mobile-logo"
-                            >
+                        <!-- Center: Logo -->
+                        <div class="col-auto text-center">
+                            <a class="navbar-brand section-link" href="{{ route('home') }}">
+                                <img src="{{ asset('storage/' . $siteData['header-logo']) }}" data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}" alt="Nile Misr" class="default-logo">
+                                <img src="{{ asset('storage/' . $siteData['header-logo']) }}" data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}" alt="Nile Misr" class="alt-logo">
+                                <img src="{{ asset('storage/' . $siteData['header-logo']) }}" data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}" alt="Nile Misr" class="mobile-logo">
+                            </a>
+                        </div>
 
-                        </a>
-
-                    </div>
-
-                    <!-- Navigation -->
-                    <div class="col-auto menu-order position-static">
-
-                        <button
-                            class="navbar-toggler float-start"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav"
-                            aria-controls="navbarNav"
-                            aria-label="Toggle navigation"
-                        >
-                            <span class="navbar-toggler-line"></span>
-                            <span class="navbar-toggler-line"></span>
-                            <span class="navbar-toggler-line"></span>
-                            <span class="navbar-toggler-line"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarNav">
-
-                            <ul class="navbar-nav">
-
-                                <li class="nav-item">
-                                    <a href="{{ route('home') }}" class="nav-link">
-                                        الرئيسية
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('about') }}" class="nav-link">
-                                        من نحن؟
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('services') }}" class="nav-link">
-                                        خدماتنا
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('moderators') }}" class="nav-link">
-                                        المشرفين
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('hajj.index') }}" class="nav-link">
-                                        برامج الحج
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('umrah.index') }}" class="nav-link">
-                                        برامج العمرة
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('whyus') }}" class="nav-link">
-                                        لماذا نيل مصر؟
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('reviews') }}" class="nav-link">
-                                        أراء العملاء
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('media') }}" class="nav-link">
-                                        المركز الإعلامي
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('contact') }}" class="nav-link">
-                                        تواصل معنا
-                                    </a>
-                                </li>
-
-                            </ul>
-
+                        <!-- Right: Book Button (desktop only, hides at 1150px) -->
+                        <div class="header-icon header-desktop-only">
+                            <div class="">
+                                <div class="header-icon">
+                                    <div class="header-button">
+                                        <a
+                                            href="javascript:void(0);"
+                                            class="btn btn-small btn-yellow btn-rounded"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#quickBookingModal"
+                                        >
+                                            احـجــــــز الآن
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
 
-                    <!-- Right side -->
-                    <div class="col-auto col-xxl-2 col-lg-2 text-end d-none d-sm-flex">
+                    <!-- Navigation Menu Row -->
+                    <div class="col-12 d-flex align-items-center justify-content-between position-static border-top border-color-extra-medium-gray md-border-none">
+                        <div class="col-auto m-auto">
 
-                        <div class="header-icon">
+                            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
 
-                            <div class="d-none d-xxl-inline-block me-25px xxl-me-10px">
+                                <ul class="navbar-nav">
 
-                                <div class="fs-15 xl-fs-13 widget-text fw-500">
+                                    <li class="nav-item">
+                                        <a href="{{ route('home') }}" class="nav-link">الرئيسية</a>
+                                    </li>
 
-                                    <span class="w-35px h-35px bg-yellow d-inline-block lh-36 me-10px border-radius-100px">
-                                        <i class="feather icon-feather-phone me-10px"></i>
-                                    </span>
+                                    <li class="nav-item">
+                                        <a href="{{ route('about') }}" class="nav-link">من نحن؟</a>
+                                    </li>
 
+                                    <li class="nav-item">
+                                        <a href="{{ route('services') }}" class="nav-link">خدماتنا</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('moderators') }}" class="nav-link">المشرفين</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('hajj.index') }}" class="nav-link">برامج الحج</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('umrah.index') }}" class="nav-link">برامج العمرة</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('whyus') }}" class="nav-link">لماذا نيل مصر؟</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('reviews') }}" class="nav-link">أراء العملاء</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('media') }}" class="nav-link">المركز الإعلامي</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('contact') }}" class="nav-link">تواصل معنا</a>
+                                    </li>
+
+                                </ul>
+
+                                <!-- Mobile: hotline + book button inside menu (shows at 1150px and below) -->
+                                <div class="header-mobile-only d-flex align-items-center justify-content-start pe-2 gap-3 py-3 border-top border-color-extra-medium-gray mt-2">
+                                    <div class="fs-15 widget-text fw-500">
+                                        <span class="w-35px h-35px bg-yellow d-inline-flex align-items-center justify-content-center me-10px border-radius-100px">
+                                            <i class="feather icon-feather-phone"></i>
+                                        </span>
+                                        <a href="tel:+{{$siteData['hotline']}}" class="widget-text fw-600 fs-18">{{$siteData['hotline']}}</a>
+                                    </div>
                                     <a
-                                        href="tel:+{{$siteData['hotline']}}"
-                                        class="widget-text text-white-hover fw-600 fs-18"
+                                        href="javascript:void(0);"
+                                        class="btn btn-small btn-yellow btn-rounded"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#quickBookingModal"
                                     >
-                                        {{$siteData['hotline']}}
+                                        احـجــــــز الآن
                                     </a>
-
                                 </div>
 
                             </div>
-
-                            <div class="header-button">
-
-                                <a
-                                    href="javascript:void(0);"
-                                    class="btn btn-small btn-yellow btn-rounded"
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#quickBookingModal"
-                                >
-                                    احـجــــــز الآن
-                                </a>
-
-                            </div>
-
                         </div>
-
                     </div>
 
                 </div>
-
             </nav>
             <!-- end navigation -->
 
