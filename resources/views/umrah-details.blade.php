18145d77a0d49f11777408ea917257327b77cc41
sofalotfy <lotfysofa@gmail.com>
2026-08-30 12:19:42 +0300

    as

--- resources/views/umrah-details.blade.php
+++ resources/views/umrah-details.blade.php
@@ -781,7 +781,7 @@
                                             {{ $loop->last ? 'cpt-rounded-tl' : '' }}
                                         ">
 
-                                            {{ $price->type }}
+                                            {{ $price->type->value() }}
 
                                         </td>
 
