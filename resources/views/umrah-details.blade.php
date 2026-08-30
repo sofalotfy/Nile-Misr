b5cc7a2ee4abbdc4c4dc734994bf3f7a5e73ad78
sofalotfy <lotfysofa@gmail.com>
2026-08-30 10:53:24 +0300

    as

--- resources/views/umrah-details.blade.php
+++ resources/views/umrah-details.blade.php
@@ -73,7 +73,7 @@
 <!-- start page title -->
 <section class="ipad-top-space-margin">
 
-    <div class="container">
+    <div class="container mt-4">
 
         <div class="row align-items-center justify-content-center">
 
