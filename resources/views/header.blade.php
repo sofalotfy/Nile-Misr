e0f5f9460a59084ea270e3988edd1630c6973d3b
AliOmar104 <aliomer22022@gmail.com>
2026-09-01 00:22:14 +0300

    Update project design

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -25,16 +25,16 @@
         <link rel="stylesheet" href="{{ asset('css/responsive.css') }}"/>
         <link rel="stylesheet" href="{{ asset('demos/hotel-and-resort/hotel-and-resort.css') }}"/>
 
-<script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/vendors.min.js') }}"></script>
-        
+        <script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/vendors.min.js') }}"></script>
 
-<script src="{{ asset('revolution/js/jquery.themepunch.tools.min.js') }}"></script>
-<script src="{{ asset('revolution/js/jquery.themepunch.revolution.min.js') }}"></script>
 
-<script src="{{ asset('revolution/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
-<script src="{{ asset('revolution/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
-<script src="{{ asset('revolution/js/extensions/revolution.extension.parallax.min.js') }}"></script>
+        <script src="{{ asset('revolution/js/jquery.themepunch.tools.min.js') }}"></script>
+        <script src="{{ asset('revolution/js/jquery.themepunch.revolution.min.js') }}"></script>
+
+        <script src="{{ asset('revolution/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
+        <script src="{{ asset('revolution/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
+        <script src="{{ asset('revolution/js/extensions/revolution.extension.parallax.min.js') }}"></script>
     </head>
 
     <body data-mobile-nav-style="classic">
@@ -57,21 +57,118 @@
         </script>
 
         <!-- start header -->
-        <header>
-
+        <header class="header-with-topbar">
+            <div class="header-top-bar top-bar-dark bg-dark-gray">
+                <div class="container-fluid">
+                    <div class="row h-45px align-items-center m-0">
+                        <div class="col-12 col-lg-7 fw-500 justify-content-lg-start justify-content-center">
+                            <span class="me-25px fs-15 md-m-0">
+                                <i class="feather icon-feather-phone-call text-base-color me-10px"></i><span class="text-light-gray">Phone: 1 800 222 000 - Any time 24/7</span>
+                            </span>
+                            <span class="d-xl-inline-block d-none fs-15"><i class="feather icon-feather-mail text-base-color me-10px"></i><a href="mailto:no-reply@domain.com" class="widget text-light-gray text-white-hover">no-reply@domain.com</a></span>
+                        </div>
+                        <div class="col-md-5 text-end d-none d-lg-flex fs-15">
+                            <a href="http://www.facebook.com" target="_blank" class="me-25px lg-me-15px">Facebook</a>
+                            <a href="http://www.twitter.com" target="_blank" class="me-25px lg-me-15px">Twitter</a>
+                            <a href="https://in.pinterest.com/" target="_blank" class="me-25px lg-me-15px">Pinterest</a>
+                            <a href="https://www.instagram.com" target="_blank">Instagram</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <!-- start navigation -->
-            <nav
-                class="navbar navbar-expand-lg header-light bg-white top-logo disable-fixed header-demo"
-            >
+            <nav class="navbar navbar-expand-lg header-light bg-white header-reverse" data-header-hover="light">
+                <div class="container-fluid">
+                    <div class="col-auto">
+                        <a class="navbar-brand" href="{{ route('home') }}">
+                            <img src="{{ asset('storage/' . $siteData['header-logo']) }}" data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}" alt="" class="default-logo">
+                            <img src="{{ asset('storage/' . $siteData['header-logo']) }}" data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}" alt="" class="alt-logo">
+                            <img src="{{ asset('storage/' . $siteData['header-logo']) }}" data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}" alt="" class="mobile-logo">
+                        </a>
+                    </div>
+                    <div class="col-auto menu-order left-nav">
+                        <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                            <ul class="navbar-nav">
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('home') }}" class="nav-link">الرئيسية</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('about') }}" class="nav-link">من نحن؟</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('services') }}" class="nav-link">خدماتنا</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('moderators') }}" class="nav-link">المشرفين</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('hajj.index') }}" class="nav-link">برامج الحج</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('umrah.index') }}" class="nav-link">برامج العمرة</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('whyus') }}" class="nav-link">لماذا نيل مصر؟</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('reviews') }}" class="nav-link">أراء العملاء</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('media') }}" class="nav-link">المركز الإعلامي</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="{{ route('contact') }}" class="nav-link">تواصل معنا</a>
+                                    </li>
+
+                                </ul>
+                        </div>
+                    </div>
+                    <div class="col-auto ms-auto ps-lg-0 d-none d-sm-flex">
+                        <div class="header-icon">
+                            <div class="fs-15 widget-text fw-500">
+                                        <span class="w-35px h-35px bg-yellow d-inline-flex align-items-center justify-content-center me-10px border-radius-100px">
+                                            <i class="feather icon-feather-phone"></i>
+                                        </span>
+                                        <a href="tel:+{{$siteData['hotline']}}" class="widget-text fw-600 fs-18">{{$siteData['hotline']}}</a>
+                                    </div>
+                                    <a href="javascript:void(0);" class="btn btn-small btn-yellow btn-rounded" data-bs-toggle="modal" data-bs-target="#quickBookingModal">
+                                        احـجــــــز الآن
+                                    </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- end navigation -->
+        </header>
+        <!-- end header -->
+
+        <!-- start header -->
+        <!-- <header>
+
+            <nav class="navbar navbar-expand-lg header-light bg-white top-logo disable-fixed header-demo">
 
                 <div class="container-fluid flex-wrap">
 
                     <div class="col-12 col-sm-12 col-lg-8 p-0 mx-lg-auto d-flex align-items-center justify-content-between">
 
-                        <!-- Left: Hotline + Toggler -->
                         <div class="col col-lg-2">
 
-                            <!-- Hotline (desktop only, hides at 1150px) -->
                             <div class="header-icon header-desktop-only">
 
                                 <div>
@@ -93,7 +190,6 @@
                                 </div>
                             </div>
 
-                            <!-- Navbar Toggler (mobile) -->
                             <button
                                 class="navbar-toggler float-start"
                                 type="button"
@@ -110,7 +206,6 @@
 
                         </div>
 
-                        <!-- Center: Logo -->
                         <div class="col-auto text-center">
                             <a class="navbar-brand section-link" href="{{ route('home') }}">
                                 <img src="{{ asset('storage/' . $siteData['header-logo']) }}" data-at2x="{{ asset('storage/' . $siteData['header-logo']) }}" alt="Nile Misr" class="default-logo">
@@ -119,7 +214,6 @@
                             </a>
                         </div>
 
-                        <!-- Right: Book Button (desktop only, hides at 1150px) -->
                         <div class="header-icon header-desktop-only">
                             <div class="">
                                 <div class="header-icon">
@@ -139,7 +233,6 @@
 
                     </div>
 
-                    <!-- Navigation Menu Row -->
                     <div class="col-12 d-flex align-items-center justify-content-between position-static border-top border-color-extra-medium-gray md-border-none">
                         <div class="col-auto m-auto">
 
@@ -189,7 +282,6 @@
 
                                 </ul>
 
-                                <!-- Mobile: hotline + book button inside menu (shows at 1150px and below) -->
                                 <div class="header-mobile-only d-flex align-items-center justify-content-start pe-2 gap-3 py-3 border-top border-color-extra-medium-gray mt-2">
                                     <div class="fs-15 widget-text fw-500">
                                         <span class="w-35px h-35px bg-yellow d-inline-flex align-items-center justify-content-center me-10px border-radius-100px">
@@ -197,12 +289,7 @@
                                         </span>
                                         <a href="tel:+{{$siteData['hotline']}}" class="widget-text fw-600 fs-18">{{$siteData['hotline']}}</a>
                                     </div>
-                                    <a
-                                        href="javascript:void(0);"
-                                        class="btn btn-small btn-yellow btn-rounded"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#quickBookingModal"
-                                    >
+                                    <a href="javascript:void(0);" class="btn btn-small btn-yellow btn-rounded" data-bs-toggle="modal" data-bs-target="#quickBookingModal">
                                         احـجــــــز الآن
                                     </a>
                                 </div>
@@ -213,7 +300,6 @@
 
                 </div>
             </nav>
-            <!-- end navigation -->
 
-        </header>
+        </header> -->
         <!-- end header -->
